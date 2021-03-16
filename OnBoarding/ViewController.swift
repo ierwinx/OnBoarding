338527840b7bcd09ea291b84c2b59c83d7965812
Erwin <ierwinx@gmail.com>
2021-03-16 14:28:53 -0600

    se inicializa proyecto

--- OnBoarding/ViewController.swift
+++ OnBoarding/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  OnBoarding
-//
-//  Created by Erwin on 16/03/21.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
 }
-
